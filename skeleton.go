63535e865ef27c2e624ff5e427fb108395f4e000
Ian Partridge <i.partridge@uk.ibm.com>
2020-01-10 15:12:43 +0000

    session 1

--- skeleton.go
+++ skeleton.go
@@ -1,5 +1,17 @@
 package skeleton
 
-func Thing() int {
-	return -1
+import "errors"
+
+var total int
+
+func Bowl(value int) error {
+	if value > 10 || value < 0 {
+		return errors.New("Value must be between 0 and 10")
+	}
+	total += value
+	return nil
+}
+
+func Score() int {
+	return total
 }
--- skeleton_test.go
+++ skeleton_test.go
@@ -1,12 +1,37 @@
-package skeleton_test
+package skeleton
 
 import (
-	. "skeleton"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestThingReturnsZero(t *testing.T) {
-	assert.Equal(t, 0, Thing())
+func playGame(pins int) error {
+	for i := 0; i < 20; i++ {
+		error := Bowl(pins)
+		if error != nil {
+			return error
+		}
+	}
+	return nil
+}
+
+func TestBowlAllZeros(t *testing.T) {
+	playGame(0)
+	assert.Equal(t, 0, Score())
+}
+
+func TestBowlAllOnes(t *testing.T) {
+	playGame(1)
+	assert.Equal(t, 20, Score())
+}
+
+func TestBowlElevenFails(t *testing.T) {
+	result := Bowl(11)
+	assert.Error(t, result)
+}
+
+func TestBowlNegativeFails(t *testing.T) {
+	result := Bowl(-1)
+	assert.Error(t, result)
 }
